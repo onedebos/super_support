8746840597a3f28a64bcb59d0bcd4c61034eaed4
Adebola <onedebos@gmail.com>
2020-06-09 14:22:47 +0100

    restrict set_user methods

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TicketsController < ApplicationController
     before_action :set_ticket, only: [:show, :update, :destroy]
-    before_action :set_user
+    before_action :set_user, only: [:create, :update, :destroy]
   def index
     tickets = Ticket.all.order(created_at: :desc)
     render json: tickets
