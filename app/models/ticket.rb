c3a9a99a8575dd8ba836fb1fed3af8ff8485e7d6
Adebola <onedebos@gmail.com>
2020-06-09 11:36:27 +0100

    add validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :user
-  belongs_to :comment
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true, uniqueness: true
+    validates_presence_of :password_digest, :name
 end
