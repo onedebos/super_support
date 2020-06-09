bd4d239c6f980ae796a482c8eafadb97f9902e0a
Adebola <onedebos@gmail.com>
2020-06-09 13:02:23 +0100

    complete tickets controller

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::TicketsController < ApplicationController
+    before_action :set_ticket, only: [:show, :update, :destroy]
   def index
     tickets = Ticket.all.order(created_at: :desc)
     render json: tickets
@@ -7,14 +8,33 @@ class Api::V1::TicketsController < ApplicationController
   def create
     ticket = Ticket.create!(ticket_params)
     if ticket
-        render json: ticket
+        render json: {ticket: ticket } 
     else
-        render json: ticket.errors
+        render json: {error: ticket.errors}
     end
   end
 
   def show
-    ticket = Ticket.find(params[:id])
-    render json: ticket
+    render json: {ticket: @ticket}
   end
+
+  def update
+    @ticket.update(ticket_params)
+    render json: {ticket: @ticket}
+  end
+  
+  def destroy
+    @ticket.destroy
+    head :no_content
+  end
+
+  private
+    def ticket_params
+        params.permit(:completed, :user_id, :comment_id)
+    end
+  
+    def set_ticket
+        @ticket = Ticket.find(params[:id])
+    end
+
 end
